052ca4c1065b5bc07801b407bdf8719657c9d3ae
loof <ypeissard@gmail.com>
2024-09-26 00:16:50 +0200

    Messing around with useShoppingCart

--- app/atoms/shopping-cart-atom.ts
+++ app/atoms/shopping-cart-atom.ts
@@ -1,3 +1,3 @@
 import { atom } from 'jotai'
 
-export const cartAtom = atom(0)
+export const cartAtom = atom({})
--- app/hooks/shopping-cart.ts
+++ app/hooks/shopping-cart.ts
@@ -8,10 +8,9 @@ import {update} from "next-auth/lib/actions";
 const LOCAL_STORAGE_KEY = 'shopping_cart';
 
 export function useShoppingCart() {
-    const [numberOfItems, setNumberOfItems] = useAtom(cartAtom);
+    const [cart, setCart] = useAtom(cartAtom);
     const { data: session, status } = useSession();
     const isAuthenticated = status === 'authenticated';
-    const [shoppingCartItems, setShoppingCartItems] = useState([]);
     const [isSyncing, setIsSyncing] = useState(false);  // To prevent multiple API calls
 
     // Load cart from local storage for unauthenticated users or server for authenticated users
@@ -21,53 +20,25 @@ export function useShoppingCart() {
                 const savedCart = localStorage.getItem(LOCAL_STORAGE_KEY);
                 if (savedCart) {
                     const parsedCart = JSON.parse(savedCart);
-                    setShoppingCartItems(parsedCart);
-                    setNumberOfItems(parsedCart.length);  // Set number based on distinct items
+                    setCart(parsedCart);
                 }
             } else if (isAuthenticated && session.user.accessToken && session.user.cartId) {
                 try {
-                    let savedCart = await getCartItems(session.user.accessToken, session.user.cartId);
-                    const savedCartLocalStorage = localStorage.getItem(LOCAL_STORAGE_KEY);
-                    let combinedCart = savedCart;
+                    let dbCart = await getCartItems(session.user.accessToken, session.user.cartId);
+                    const localCart = localStorage.getItem(LOCAL_STORAGE_KEY);
+                    let combinedCart = dbCart;
 
-                    if (savedCartLocalStorage) {
-                        const parsedLocalCart = JSON.parse(savedCartLocalStorage);
+                    if (localCart) {
+                        const parsedLocalCart = JSON.parse(localCart);
 
-                        let newLocalItems = parsedLocalCart.filter((localCartItem) =>
-                            !savedCart.some((dbItem) =>
-                                dbItem.eventId === localCartItem.eventId && dbItem.variantId === localCartItem.variantId
-                            )
-                        );
-                        newLocalItems = newLocalItems.map((newItem) => ({ ...newItem, cartId: session.user.cartId }));
-
-                        if (newLocalItems.length > 0) {
-                            await createCartItems(session.user.accessToken, session.user.cartId, newLocalItems);
+                        if (parsedLocalCart.length > 0) {
+                            const cart =  await createCartItems(session.user.accessToken, session.user.cartId, parsedLocalCart);
+                            setCart(cart)
                         }
 
-                        let existingLocalItems = parsedLocalCart.filter((localCartItem) =>
-                            savedCart.some((dbItem) =>
-                                dbItem.eventId === localCartItem.eventId && dbItem.variantId === localCartItem.variantId
-                            )
-                        );
-                        savedCart = savedCart.map((savedCartItem) => {
-                            let existingItem = existingLocalItems.find((existingItem) =>
-                                existingItem.eventId === savedCartItem.eventId && existingItem.variantId === savedCartItem.variantId
-                            );
-                            if (existingItem) {
-                                savedCartItem.amount += existingItem.amount;
-                                updateCartItem(session.user.accessToken, savedCartItem);
-
-                            }
-                            return savedCartItem;
-                        });
-
-                        // Merge server and local cart items
-                        combinedCart = [...savedCart, ...newLocalItems];
                         localStorage.removeItem(LOCAL_STORAGE_KEY);
                     }
 
-                    setShoppingCartItems(combinedCart);
-                    setNumberOfItems(combinedCart.length);  // Set number based on distinct items
                 } catch (error) {
                     console.error("Error fetching cart items", error);
                 }
@@ -79,52 +50,47 @@ export function useShoppingCart() {
 
     // Save cart to local storage for unauthenticated users
     useEffect(() => {
-        if (!isAuthenticated && shoppingCartItems.length > 0) {
-            localStorage.setItem(LOCAL_STORAGE_KEY, JSON.stringify(shoppingCartItems));
+        async function loadData() {
+
+            if (!isAuthenticated && cart.items.length > 0) {
+                localStorage.setItem(LOCAL_STORAGE_KEY, JSON.stringify(cart));
+            }
         }
-    }, [shoppingCartItems, isAuthenticated]);
+
+        loadData()
+    }, [cart, isAuthenticated]);
 
     // Add or update item in the shopping cart
     const addShoppingCartItem = async (cartItem) => {
-        const existingItem = shoppingCartItems.find(
+        const existingItem = cart.items.find(
             (item) => item.eventId === cartItem.eventId && item.variantId === cartItem.variantId
         );
 
-        if (!existingItem) {
-            setNumberOfItems(prev => prev + 1);
-        }
-
-        setShoppingCartItems((prevCartItems) => {
-            const updatedCartItems = [...prevCartItems];
-            const existingItemIndex = updatedCartItems.findIndex(
-                (item) => item.eventId === cartItem.eventId && item.variantId === cartItem.variantId
-            );
-
-            if (existingItemIndex !== -1) {
-                updatedCartItems[existingItemIndex].amount += cartItem.amount;
-            } else {
-                updatedCartItems.push(cartItem);
-            }
-
-            return updatedCartItems;
-        });
-
-        if (isAuthenticated && session?.user?.accessToken && session?.user?.cartId && !isSyncing) {
+        if (isAuthenticated && session?.user?.accessToken && session?.user?.cartId && !isSyncing){
             setIsSyncing(true);  // Start syncing process
             try {
-                if (existingItem) {
-                    await updateCartItem(session.user.accessToken, {
-                        ...existingItem,
-                        amount: existingItem.amount + cartItem.amount
-                    });
-                } else {
-                    await createCartItems(session.user.accessToken, session.user.cartId, [cartItem]);
-                }
+                const newCart = await createCartItems(session.user.accessToken, session.user.cartId, [cart.items]);
+                setCart(newCart)
             } catch (error) {
                 console.error("Error syncing cart item to server", error);
             } finally {
                 setIsSyncing(false);  // Syncing finished
             }
+        } else if (!isAuthenticated) {
+            setCart((prevCart) => {
+                const updatedCart = [...prevCart];
+                const existingItemIndex = updatedCart.items.findIndex(
+                    (item) => item.eventId === cartItem.eventId && item.variantId === cartItem.variantId
+                );
+
+                if (existingItemIndex !== -1) {
+                    updatedCart.items[existingItemIndex].amount += cartItem.amount;
+                } else {
+                    updatedCart.push(cartItem);
+                }
+
+                return updatedCart;
+            });
         }
     };
 
@@ -135,7 +101,7 @@ export function useShoppingCart() {
             const removedItem = prevCartItems.find((item) => item.id === id);
 
             if (removedItem) {
-                setNumberOfItems((prev) => prev - 1);
+                setCart((prev) => prev - 1);
             }
 
             return updatedCartItems;
