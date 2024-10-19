848ffe0c8d598dafe5a021010a5337b6b2fca8ba
loof <ypeissard@gmail.com>
2024-10-19 12:56:18 +0200

    Hides Checkout when no items in cart

--- app/cart/page.tsx
+++ app/cart/page.tsx
@@ -1,17 +1,5 @@
-import { MinusCircle, PlusCircle, Trash2 } from "lucide-react"
-import { Button } from "@/components/ui/button"
-import {roundToFiveCents} from "@/lib/utils";
-import {MinusIcon, PlusIcon} from "@/app/icons/icons";
-import CartPageComponent from "@/components/cart/overview";
 import CartOverview from "@/components/cart/overview";
 
-interface CartItem {
-    id: number
-    name: string
-    price: number
-    amount: number
-    variant: string
-}
 
 export default function CartPage() {
     return <CartOverview/>
--- components/checkout/checkout.tsx
+++ components/checkout/checkout.tsx
@@ -18,15 +18,10 @@ import {
     AccordionItem,
     AccordionTrigger
 } from "@/components/ui/accordion"
+import {useShoppingCart} from "@/app/hooks/use-shopping-cart";
 
 export default function Checkout() {
-    const [isAddressExpanded, setIsAddressExpanded] = useState(false)
-
-    // This would typically come from your application's state or API
-    const cartItems = [
-        {id: 1, name: "Rohkaffee Bio & Fair: Tansania Robusta", variant: "320g", price: 29.99, quantity: 2},
-        {id: 2, name: "Herbströstung 2024", variant: "560g", price: 59.99, quantity: 1},
-    ]
+    const { cart, addShoppingCartItem, removeShoppingCartItem } = useShoppingCart();
 
     const shippingAddress = {
         name: "Yves Peissard",
@@ -37,7 +32,7 @@ export default function Checkout() {
         country: "Schweiz"
     }
 
-    const subtotal = cartItems.reduce((sum, item) => sum + item.price * item.quantity, 0)
+    const subtotal = cart.items.reduce((sum, item) => sum + item.variant?.price * item.amount, 0)
     const shippingCost = 5.99
     const tax = subtotal * 0.08 // Assuming 8% tax rate
     const total = subtotal + shippingCost + tax
@@ -50,77 +45,77 @@ export default function Checkout() {
                 <div className="md:col-span-2">
                     <Card>
                         <CardContent className={"mt-8"}>
-                            {cartItems.map((item) => (
-                                <div key={item.id} className="flex justify-between items-start mb-4">
-                                    <div className={"flex flex-col gap-1"}>
-                                        <h3 className={"text-2xl mt-2 max-w-[300px]"}>{item.name}</h3>
-                                        <p className="text-xl text-muted-foreground">{item.variant}</p>
-                                        <p className="text-lg">Anzahl: {item.quantity}</p>
+                            {cart.items.length === 0 ? (
+                                <p className="text-center text-xl">Keine Produkte im Warenkorb</p>
+                            ) : (
+                                cart.items.map((item) => (
+                                    <div key={item.id} className="flex justify-between items-start mb-4">
+                                        <div className={"flex flex-col gap-1"}>
+                                            <h3 className={"text-2xl mt-2 max-w-[300px]"}>{item.variant?.productName}</h3>
+                                            <p className="text-xl text-muted-foreground">{item.variant?.name}</p>
+                                            <p className="text-lg">Anzahl: {item.amount}</p>
+                                        </div>
+                                        <p className={"text-2xl"}>CHF {(item.variant?.price * item.amount).toFixed(2)}</p>
                                     </div>
-                                    <p className={"text-2xl"}>CHF {(item.price * item.quantity).toFixed(2)}</p>
-                                </div>
-                            ))}
+                                ))
+                            )}
                         </CardContent>
                     </Card>
 
-                    <Card className={"mt-12"}>
-                        <CardHeader>
-                            <CardTitle className="text-2xl">Lieferadresse</CardTitle>
-                        </CardHeader>
-                        <CardContent className={"text-sm"}>
-
-
-                            {/*<div className="flex items-center">
-                                <MapPin className="mr-2 h-4 w-4"/>
-                                {shippingAddress.name}, {shippingAddress.city}
-                            </div>*/}
-
-                            <p className={"text-xl"}>{shippingAddress.street}</p>
-                            <p className={"text-xl"}> {shippingAddress.city}, {shippingAddress.state} {shippingAddress.zip}</p>
-                            <p className={"text-xl"}>{shippingAddress.country}</p>
-
-                        </CardContent>
-                    </Card>
+                    {cart.items.length > 0 && (
+                        <Card className={"mt-12"}>
+                            <CardHeader>
+                                <CardTitle className="text-2xl">Lieferadresse</CardTitle>
+                            </CardHeader>
+                            <CardContent className={"text-sm"}>
+                                <p className={"text-xl"}>{shippingAddress.street}</p>
+                                <p className={"text-xl"}> {shippingAddress.city}, {shippingAddress.state} {shippingAddress.zip}</p>
+                                <p className={"text-xl"}>{shippingAddress.country}</p>
+                            </CardContent>
+                        </Card>
+                    )}
                 </div>
 
-                <div>
-                    <Card>
-                        <CardHeader>
-                            <CardTitle className="text-2xl">Bestellung Total</CardTitle>
-                        </CardHeader>
-                        <CardContent>
-                            <div className="space-y-2">
-                                <div className="flex justify-between">
-                                    <span className={"text-lg"}>Subtotal</span>
-                                    <span>CHF {subtotal.toFixed(2)}</span>
+                {cart.items.length > 0 && (
+                    <div>
+                        <Card>
+                            <CardHeader>
+                                <CardTitle className="text-2xl">Bestellung Total</CardTitle>
+                            </CardHeader>
+                            <CardContent>
+                                <div className="space-y-2">
+                                    <div className="flex justify-between">
+                                        <span className={"text-lg"}>Subtotal</span>
+                                        <span>CHF {subtotal.toFixed(2)}</span>
+                                    </div>
+                                    <div className="flex justify-between">
+                                        <span className={"text-lg mb-2"}>Versand</span>
+                                        <span>CHF {shippingCost.toFixed(2)}</span>
+                                    </div>
+                                    <Separator/>
+                                    <div className="flex justify-between font-bold">
+                                        <span className={"text-2xl mt-2"}>Total</span>
+                                        <span className={"text-2xl mt-2"}>CHF {total.toFixed(2)}</span>
+                                    </div>
                                 </div>
-                                <div className="flex justify-between">
-                                    <span className={"text-lg mb-2"}>Versand</span>
-                                    <span>CHF {shippingCost.toFixed(2)}</span>
-                                </div>
-                                <Separator/>
-                                <div className="flex justify-between font-bold">
-                                    <span className={"text-2xl mt-2"}>Total</span>
-                                    <span className={"text-2xl mt-2"}>CHF {total.toFixed(2)}</span>
-                                </div>
-                            </div>
-                        </CardContent>
-                        <CardFooter>
-                            <Button className="w-full">
-                                <CreditCard className="mr-2 h-4 w-4"/>Verbindlich bestellen
-                            </Button>
-                        </CardFooter>
-                    </Card>
+                            </CardContent>
+                            <CardFooter>
+                                <Button className="w-full">
+                                    <CreditCard className="mr-2 h-4 w-4"/>Verbindlich bestellen
+                                </Button>
+                            </CardFooter>
+                        </Card>
 
-                    <Card className="mt-12">
-                        <CardContent className="pt-6">
-                            <div className="flex items-center text-sm text-muted-foreground">
-                                <Truck className="mr-6 h-8 w-8"/>
-                                <span className={"text-xl"}>Geschätzte Lieferzeit: 3-5 Werktage</span>
-                            </div>
-                        </CardContent>
-                    </Card>
-                </div>
+                        <Card className="mt-12">
+                            <CardContent className="pt-6">
+                                <div className="flex items-center text-sm text-muted-foreground">
+                                    <Truck className="mr-6 h-8 w-8"/>
+                                    <span className={"text-xl"}>Geschätzte Lieferzeit: 3-5 Werktage</span>
+                                </div>
+                            </CardContent>
+                        </Card>
+                    </div>
+                )}
             </div>
         </div>
     )
